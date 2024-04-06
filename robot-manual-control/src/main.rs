6fcfb5293ec434ff7f8732df470950981f4dcb02
Christopher Head <chead@chead.ca>
2024-04-06 13:15:24 -0700

    robot-manual-control: delete unnecessary loop

--- robot-manual-control/src/main.rs
+++ robot-manual-control/src/main.rs
@@ -87,101 +87,99 @@ impl Application {
 			.set_foreground(gpu::Colour::Rgb(Rgb(0xFF_FF_FF)))
 			.await?;
 
-		loop {
-			// Display main menu.
-			const HELP_LINES: [Option<&str>; 10] = [
-				Some("Robot Manual Control Main Menu"),
-				None,
-				None,
-				None,
-				Some("W/S: Move Forward/Back"),
-				Some("A/D: Turn Left/Right"),
-				Some("Q/E: Move Down/Up"),
-				Some("I: Select Inventory Slot"),
-				Some("R: Reboot"),
-				Some("P: Power Down"),
-			];
-			let mut gpu_locked = self.gpu.lock(&mut self.invoker, &mut self.buffer);
-			gpu_locked
-				.fill(Point { x: 1, y: 1 }, screen_size, ' ')
-				.await?;
-			for (index, line) in HELP_LINES.iter().enumerate() {
-				if let Some(line) = line {
-					#[allow(clippy::cast_possible_truncation)] // Line numbers are not that big.
-					gpu_locked
-						.set(
-							Point {
-								x: 1,
-								y: (index + 1) as u32,
-							},
-							line,
-							gpu::TextDirection::Horizontal,
-						)
-						.await?;
-				}
-			}
-
-			loop {
-				// Display live energy level.
-				let mut gpu_locked = self.gpu.lock(&mut self.invoker, &mut self.buffer);
-				#[allow(clippy::cast_possible_truncation)] // Energy levels are not that big.
+		// Display main menu.
+		const HELP_LINES: [Option<&str>; 10] = [
+			Some("Robot Manual Control Main Menu"),
+			None,
+			None,
+			None,
+			Some("W/S: Move Forward/Back"),
+			Some("A/D: Turn Left/Right"),
+			Some("Q/E: Move Down/Up"),
+			Some("I: Select Inventory Slot"),
+			Some("R: Reboot"),
+			Some("P: Power Down"),
+		];
+		let mut gpu_locked = self.gpu.lock(&mut self.invoker, &mut self.buffer);
+		gpu_locked
+			.fill(Point { x: 1, y: 1 }, screen_size, ' ')
+			.await?;
+		for (index, line) in HELP_LINES.iter().enumerate() {
+			if let Some(line) = line {
+				#[allow(clippy::cast_possible_truncation)] // Line numbers are not that big.
 				gpu_locked
 					.set(
-						Point { x: 1, y: 3 },
-						&format!(
-							"Energy: {}%   ",
-							computer::energy().into_inner() as i64 * 100
-								/ computer::max_energy().into_inner() as i64
-						),
+						Point {
+							x: 1,
+							y: (index + 1) as u32,
+						},
+						line,
 						gpu::TextDirection::Horizontal,
 					)
 					.await?;
+			}
+		}
 
-				// Handle any signals.
-				sleep::until_signal().await;
-				while let Some(len) = computer::pull_signal_length() {
-					// Pop and handle signal.
-					self.buffer.resize(len.get(), 0);
-					let signal = computer::pull_signal(&mut self.buffer)?.unwrap();
-					let mut decoder = Decoder::new(signal);
-					let signal_name = decoder
-						.str()
+		loop {
+			// Display live energy level.
+			let mut gpu_locked = self.gpu.lock(&mut self.invoker, &mut self.buffer);
+			#[allow(clippy::cast_possible_truncation)] // Energy levels are not that big.
+			gpu_locked
+				.set(
+					Point { x: 1, y: 3 },
+					&format!(
+						"Energy: {}%   ",
+						computer::energy().into_inner() as i64 * 100
+							/ computer::max_energy().into_inner() as i64
+					),
+					gpu::TextDirection::Horizontal,
+				)
+				.await?;
+
+			// Handle any signals.
+			sleep::until_signal().await;
+			while let Some(len) = computer::pull_signal_length() {
+				// Pop and handle signal.
+				self.buffer.resize(len.get(), 0);
+				let signal = computer::pull_signal(&mut self.buffer)?.unwrap();
+				let mut decoder = Decoder::new(signal);
+				let signal_name = decoder
+					.str()
+					.map_err(|_| oc_wasm_safe::error::Error::CborDecode)?;
+				if signal_name == keyboard::KeySignal::KEY_DOWN {
+					let data: keyboard::KeySignal<'_> = decoder
+						.decode()
 						.map_err(|_| oc_wasm_safe::error::Error::CborDecode)?;
-					if signal_name == keyboard::KeySignal::KEY_DOWN {
-						let data: keyboard::KeySignal<'_> = decoder
-							.decode()
-							.map_err(|_| oc_wasm_safe::error::Error::CborDecode)?;
-						let data = data.to_basic();
-						if let Some(ch) = data.character {
-							let mut robot_locked =
-								self.robot.lock(&mut self.invoker, &mut self.buffer);
-							if ch == 'w' || ch == 'W' {
-								robot_locked.move_robot(robot::MoveDirection::Front).await?;
-							} else if ch == 's' || ch == 'S' {
-								robot_locked.move_robot(robot::MoveDirection::Back).await?;
-							} else if ch == 'a' || ch == 'A' {
-								robot_locked.turn(robot::Rotation::Counterclockwise).await?;
-							} else if ch == 'd' || ch == 'D' {
-								robot_locked.turn(robot::Rotation::Clockwise).await?;
-							} else if ch == 'q' || ch == 'Q' {
-								robot_locked.move_robot(robot::MoveDirection::Down).await?;
-							} else if ch == 'e' || ch == 'E' {
-								robot_locked.move_robot(robot::MoveDirection::Up).await?;
-							} else if ch == 'i' || ch == 'I' {
-								let slot = robot_locked.selected().await?;
-								let size = robot_locked.inventory_size().await?;
-								let slot = NonZeroU32::new(if slot.get() == size {
-									1
-								} else {
-									slot.get() + 1
-								})
-								.unwrap();
-								robot_locked.select(slot).await?;
-							} else if ch == 'r' || ch == 'R' {
-								computer::reboot();
-							} else if ch == 'p' || ch == 'P' {
-								computer::shutdown();
-							}
+					let data = data.to_basic();
+					if let Some(ch) = data.character {
+						let mut robot_locked =
+							self.robot.lock(&mut self.invoker, &mut self.buffer);
+						if ch == 'w' || ch == 'W' {
+							robot_locked.move_robot(robot::MoveDirection::Front).await?;
+						} else if ch == 's' || ch == 'S' {
+							robot_locked.move_robot(robot::MoveDirection::Back).await?;
+						} else if ch == 'a' || ch == 'A' {
+							robot_locked.turn(robot::Rotation::Counterclockwise).await?;
+						} else if ch == 'd' || ch == 'D' {
+							robot_locked.turn(robot::Rotation::Clockwise).await?;
+						} else if ch == 'q' || ch == 'Q' {
+							robot_locked.move_robot(robot::MoveDirection::Down).await?;
+						} else if ch == 'e' || ch == 'E' {
+							robot_locked.move_robot(robot::MoveDirection::Up).await?;
+						} else if ch == 'i' || ch == 'I' {
+							let slot = robot_locked.selected().await?;
+							let size = robot_locked.inventory_size().await?;
+							let slot = NonZeroU32::new(if slot.get() == size {
+								1
+							} else {
+								slot.get() + 1
+							})
+							.unwrap();
+							robot_locked.select(slot).await?;
+						} else if ch == 'r' || ch == 'R' {
+							computer::reboot();
+						} else if ch == 'p' || ch == 'P' {
+							computer::shutdown();
 						}
 					}
 				}
